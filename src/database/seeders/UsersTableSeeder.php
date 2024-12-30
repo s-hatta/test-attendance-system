5472a791cdcbcedc3f36e819b1c405294d2a676f
s-hatta <hatta.shogo@gmail.com>
2024-12-30 22:17:10 +0900

    add: 一般ユーザーのseederを追加する

---
+++ src/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+        User::factory(10)->create();
+    }
+}
