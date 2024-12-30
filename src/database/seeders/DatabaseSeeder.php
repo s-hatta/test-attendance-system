3807a2f5b0d4d701f31bf5d1f0f8fe7f84319a81
s-hatta <hatta.shogo@gmail.com>
2024-12-31 03:10:36 +0900

    add: 休憩に関するseederを追加する

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(AdminsTableSeeder::class);
         $this->call(AttendancesTableSeeder::class);
+        $this->call(BreakTimesTableSeeder::class);
     }
 }
