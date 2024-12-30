28b19dff1bbb7303e5194af1025a99eb54c4110a
s-hatta <hatta.shogo@gmail.com>
2024-12-30 21:59:09 +0900

    add: Attendanceモデルを追加

---
+++ src/app/Models/Attendance.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'date',
+        'clock_in_at',
+        'clock_out_at',
+        'status',
+    ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function breakTime()
+    {
+        return $this->hasMany(BreakTime:class);
+    }
+    
+    public function attendanceCorrection()
+    {
+        return $this->hasOne(AttendanceCorrection::class);
+    }
+}
