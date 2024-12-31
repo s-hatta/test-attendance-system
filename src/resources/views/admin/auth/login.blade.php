59bba0d4cbd492fa7f971f8037086f71d8cc7756
s-hatta <hatta.shogo@gmail.com>
2025-01-01 01:22:47 +0900

    add: 管理者ログイン用のviewを追加する

---
+++ src/resources/views/admin/auth/login.blade.php
@@ -0,0 +1,15 @@
+@extends('/layouts.common')
+@section('title','ログイン（管理者）')
+
+@section('content')
+    <form method="POST" method="route{{'admin.login'}}">
+        @csrf
+        <p>メールアドレス</p>
+        <p>{{$errors->first('email')}}</p>
+        <input class="border border-solid border-black" type="text" name="email" value="{{old('email')}}">
+        <p>パスワード</p>
+        <p>{{$errors->first('password')}}</p>
+        <input class="border border-solid border-black" type="password" name="password">
+        <button class="bg-black text-white" type="submit">ログインする</button>
+    </form>
+@endsection
