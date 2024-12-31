21c2ff239c15fed321d3ba748c02dca71b5d94d2
s-hatta <hatta.shogo@gmail.com>
2024-12-31 23:18:54 +0900

    add: ログイン画面と登録画面にエラーメッセージの表示を追加する

--- src/resources/views/user/auth/login.blade.php
+++ src/resources/views/user/auth/login.blade.php
@@ -2,11 +2,13 @@
 @section('title','ログイン')
 
 @section('content')
-    <form method="POST">
+    <form method="POST" method="route{{'user.login'}}">
         @csrf
         <p>メールアドレス</p>
+        <p>{{$errors->first('email')}}</p>
         <input class="border border-solid border-black" type="text" name="email" value="{{old('email')}}">
         <p>パスワード</p>
+        <p>{{$errors->first('password')}}</p>
         <input class="border border-solid border-black" type="password" name="password">
         <button class="bg-black text-white" type="submit">ログインする</button>
     </form>
--- src/routes/web.php
+++ src/routes/web.php
@@ -10,5 +10,7 @@ Route::get('/', [HomeController::class, 'index']);
 /* 一般ユーザー用ルート */
 Route::name('user.')->group(function () {
     Route::get('/register', [User\RegisterController::class, 'create'])->name('register');
+    Route::post('/register', [User\RegisterController::class, 'store']);
     Route::get('/login', [User\LoginController::class, 'create'])->name('login');
+    Route::post('/login', [User\LoginController::class, 'store']);
 });
