a8a7e202c800caef93da02469e3deb9ab100a604
s-hatta <hatta.shogo@gmail.com>
2025-01-02 20:18:35 +0900

    add: 勤怠修正申請登録処理を追加する

--- src/app/Http/Controllers/User/AttendanceController.php
+++ src/app/Http/Controllers/User/AttendanceController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Attendance;
+use App\Models\AttendanceCorrection;
+use App\Models\BreakTimeCorrection;
 use Illuminate\Http\Request;
 use App\Http\Requests\CorrectionRequest;
+use App\Enums\CorrectionStatus;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class AttendanceController extends Controller
 {
@@ -42,5 +46,29 @@ class AttendanceController extends Controller
             'remark' => $attendance->remark,
         ];
         return view('user.attendance.show', compact('id','param'));
+    }
+    
+    /**
+     * 勤怠修正申請登録
+     */
+    public function store(CorrectionRequest $request)
+    {
+        $user = Auth::user();
+        $validated = $request->validated();
+        $attendanceCorrection = $user->attendanceCorrections()->create([
+            'date' => $validated['date'],
+            'clock_in_at' => $validated['clock_in_at'],
+            'clock_out_at' => $validated['clock_out_at'],
+            'status' => CorrectionStatus::PENDING->value,
+            'remark' => $validated['remark'],
+        ]);
+        
+        foreach( $validated['break_times'] as $breakTime ) {
+            $attendanceCorrection->breakTimeCorrections()->create([
+                'start_at' => $breakTime['start'],
+                'end_at' => $breakTime['end'],
+            ]);
+        }
+        return redirect()->route('user.attendance.index');
     }
 }
