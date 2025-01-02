e2915c5b38ebb857c620e90ff3085f40671bec91
s-hatta <hatta.shogo@gmail.com>
2025-01-03 02:10:07 +0900

    add: 申請一覧画面表示用のアクションを定義する

--- src/app/Http/Controllers/User/CorrectionController.php
+++ src/app/Http/Controllers/User/CorrectionController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\AttendanceCorrection;
+use Illuminate\Support\Facades\Auth;
 
 class CorrectionController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('user.correction.index');
+        $status = $request->input('status', 0);
+        
+        $corrections = AttendanceCorrection::with('attendance')
+            ->where('user_id', Auth::id())
+            ->where('status', $status)
+            ->orderBy('created_at', 'desc')
+            ->get();
+            
+        return view('user.correction.index', compact('corrections','status') );
     }
 }
