0b8da8b906df7bcf39c7c174d1388da77a107ccd
s-hatta <hatta.shogo@gmail.com>
2025-01-05 22:36:06 +0900

    change: 会員登録/ログイン画面にスタイルを適用する

--- src/resources/views/admin/auth/login.blade.php
+++ src/resources/views/admin/auth/login.blade.php
@@ -1,15 +1,39 @@
-@extends('/layouts.common')
+@extends('/layouts.auth')
 @section('title','ログイン（管理者）')
 
+@php
+    $labelClass = "block text-[24px] font-bold mb-1";
+    $inputClass = "w-full text-[26px] font-bold px-3 py-2 border border-black rounded-[4px]";
+    $alermClass = "text-base font-bold text-[#f00] min-h-6";
+    $buttonClass = "w-full text-[26px] font-bold bg-black text-white py-4 px-4 mt-8 rounded-[4px] hover:bg-gray-800"
+@endphp
+
 @section('content')
-    <form method="POST" action="{{route('admin.login')}}">
-        @csrf
-        <p>メールアドレス</p>
-        <p>{{$errors->first('email')}}</p>
-        <input class="border border-solid border-black" type="text" name="email" value="{{old('email')}}">
-        <p>パスワード</p>
-        <p>{{$errors->first('password')}}</p>
-        <input class="border border-solid border-black" type="password" name="password">
-        <button class="bg-black text-white" type="submit">ログインする</button>
-    </form>
+<div class="flex justify-center">
+    <div class="w-full max-w-[680px] p-6 mt-10">
+        <h1 class="text-center text-[36px] font-bold">管理者ログイン</h1>
+        
+        <form method="POST" action="{{ route('admin.login') }}" class="space-y-6">
+            @csrf
+            {{-- メールアドレス --}}
+            <div>
+                <label class="{{$labelClass}}">メールアドレス</label>
+                <input type="text" name="email" class="{{$inputClass}}" value="{{old('email')}}">
+                <p class="{{$alermClass}}">@error('email'){{ $message }}@enderror</p>
+            </div>
+            {{-- パスワード --}}
+            <div>
+                <label class="{{$labelClass}}">パスワード</label>
+                <input type="password" name="password" class="{{$inputClass}}">
+                <p class="{{$alermClass}}">@error('password'){{ $message }}@enderror</p>
+            </div>
+            {{-- ログインボタン --}}
+            <div>
+                <button type="submit" class="{{$buttonClass}}">
+                    管理者ログインする
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
