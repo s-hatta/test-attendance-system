26cc44b2f33cd932555f848a00a8af3ea8ba1508
s-hatta <hatta.shogo@gmail.com>
2025-01-13 04:03:42 +0900

    fix; メッセージ表示方法を修正する

--- src/resources/views/user/auth/login.blade.php
+++ src/resources/views/user/auth/login.blade.php
@@ -13,8 +13,8 @@
     <div class="w-full max-w-[680px] p-6 mt-10">
         <h1 class="text-center text-[36px] font-bold">ログイン</h1>
         
-        @if(isset($message))
-            <p class="text-red-500">{{$message}}</p>
+        @if( session('message') )
+            <p class="text-red-500">{{session('message')}}</p>
         @endif
         
         <form method="POST" action="{{ route('user.login') }}" class="space-y-6">
