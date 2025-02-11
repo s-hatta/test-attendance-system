3617afa2fea4231f42acb4f74e5d999c22d99d3c
s_hatta <s_hatta@gmail.com>
2025-02-11 10:03:05 +0900

    fix: 重複して表示される可能性のあるメッセージはどちらか片方だけ出すようにする

--- src/resources/views/parts/attendance.blade.php
+++ src/resources/views/parts/attendance.blade.php
@@ -18,26 +18,26 @@
                     <div class="flex-1 text-[16px] font-bold my-auto">{{$param['name']}}</div>
                 </div>
             </div>
-            
+
             {{-- 日付 --}}
             <div class="{{ $tableRowClass }}">
                 <div class="{{ $tableHeaderClass }}">日付</div>
                 <div class="{{ $tableDataClass }}">
                     {{-- 年 --}}
-                    <input 
-                        type="text" 
-                        name="year" 
-                        value="{{$param['year']}}" 
+                    <input
+                        type="text"
+                        name="year"
+                        value="{{$param['year']}}"
                         class="year-input {{$tableTextInputClass}} @error('date_time') border-red-500 @enderror"
                         data-original-value="{{$param['year']}}"
                         @if(isset($param['is_pending'])) disabled @endif
                     >
                     <span class="{{$tableDataSpanClass}}"></span>
                     {{-- 月日 --}}
-                    <input 
-                        type="text" 
-                        name="date" 
-                        value="{{$param['date']}}" 
+                    <input
+                        type="text"
+                        name="date"
+                        value="{{$param['date']}}"
                         class="date-input {{$tableTextInputClass}} @error('date_time') border-red-500 @enderror"
                         data-original-value="{{$param['date']}}"
                         @if(isset($param['is_pending'])) disabled @endif
@@ -47,79 +47,82 @@
                     <div class="{{ $errorMessageClass }}">{{ $message }}</div>
                 @enderror
             </div>
-            
+
             {{-- 出勤：退勤 --}}
             <div class="{{ $tableRowClass }}">
                 <div class="{{ $tableHeaderClass }}">出勤・退勤</div>
                 <div class="{{ $tableDataClass }}">
                     {{-- 出勤時刻 --}}
-                    <input 
-                        type="text" 
-                        name="clock_in" 
-                        value="{{$param['clock_in']}}" 
+                    <input
+                        type="text"
+                        name="clock_in"
+                        value="{{$param['clock_in']}}"
                         class="time-input {{$tableTextInputClass}} @error('clock_in') border-red-500 @enderror"
                         data-original-value="{{$param['clock_in']}}"
                         @if(isset($param['is_pending'])) disabled @endif
                     >
                     <span class="{{$tableDataSpanClass}}">〜</span>
                     {{-- 退勤時刻 --}}
-                    <input 
-                        type="text" 
-                        name="clock_out" 
-                        value="{{$param['clock_out']}}" 
+                    <input
+                        type="text"
+                        name="clock_out"
+                        value="{{$param['clock_out']}}"
                         class="time-input {{$tableTextInputClass}} @error('clock_out') border-red-500 @enderror"
                         data-original-value="{{$param['clock_out']}}"
                         @if(isset($param['is_pending'])) disabled @endif
                     >
                 </div>
-                @error('clock_in')
-                    <div class="{{ $errorMessageClass }}">{{ $message }}</div>
-                @enderror
-                @error('clock_out')
-                    <div class="{{ $errorMessageClass }}">{{ $message }}</div>
-                @enderror
+                @if( $errors->has('clock_in') )
+                    <div class="{{ $errorMessageClass }}">{{ $errors->first('clock_in') }}</div>
+                @else
+                    @if( $errors->has('clock_out') )
+                        <div class="{{ $errorMessageClass }}">{{ $errors->first('clock_out') }}</div>
+                    @endif
+                @endif
+
             </div>
-            
+
             {{-- 休憩 --}}
             @foreach($param['break_times'] as $breakTime)
             <div class="{{ $tableRowClass }}">
                 <div class="{{ $tableHeaderClass }}">休憩</div>
                 <div class="{{ $tableDataClass }}">
                     {{-- 開始時刻 --}}
-                    <input 
-                        type="text" 
-                        name="break_times[{{$loop->index}}][start]" 
-                        value="{{$breakTime['start']}}" 
+                    <input
+                        type="text"
+                        name="break_times[{{$loop->index}}][start]"
+                        value="{{$breakTime['start']}}"
                         class="time-input {{$tableTextInputClass}} @error('break_times.'.$loop->index.'.start') border-red-500 @enderror"
                         data-original-value="{{$breakTime['start']}}"
                         @if(isset($param['is_pending'])) disabled @endif
                     >
                     <span class="{{$tableDataSpanClass}}">〜</span>
                     {{-- 終了時刻 --}}
-                    <input 
-                        type="text" 
-                        name="break_times[{{$loop->index}}][end]" 
-                        value="{{$breakTime['end']}}" 
+                    <input
+                        type="text"
+                        name="break_times[{{$loop->index}}][end]"
+                        value="{{$breakTime['end']}}"
                         class="time-input {{$tableTextInputClass}} @error('break_times.'.$loop->index.'.end') border-red-500 @enderror"
                         data-original-value="{{$breakTime['end']}}"
                         @if(isset($param['is_pending'])) disabled @endif
                     >
                 </div>
-                @error('break_times.'.$loop->index.'.start')
-                    <div class="{{ $errorMessageClass }}">{{ $message }}</div>
-                @enderror
-                @error('break_times.'.$loop->index.'.end')
-                    <div class="{{ $errorMessageClass }}">{{ $message }}</div>
-                @enderror
+                @if( $errors->has('break_times.'.$loop->index.'.start') )
+                    <div class="{{ $errorMessageClass }}">{{ $errors->first('break_times.'.$loop->index.'.start') }}</div>
+                @else
+                    @if( $errors->has('break_times.'.$loop->index.'.end') )
+                        <div class="{{ $errorMessageClass }}">{{ $errors->first('break_times.'.$loop->index.'.end') }}</div>
+                    @endif
+                @endif
             </div>
             @endforeach
-            
+
             {{-- 備考 --}}
             <div class="{{ $tableRowClass }}">
                 <div class="{{ $tableHeaderClass }}">備考</div>
                 <div class="{{ $tableDataClass }}">
-                    <textarea 
-                        name="remark" 
+                    <textarea
+                        name="remark"
                         class="{{$tableTextareaClass}} @error('remark') border-red-500 @enderror"
                         rows="3"
                         @if(isset($param['is_pending'])) disabled @endif
@@ -135,7 +138,7 @@
             @if(isset($param['is_pending']))
             <p class="text-red-500">※承認待ちのため修正はできません</p>
             @else
-            <button 
+            <button
                 type="submit"
                 class="bg-black text-white my-20 px-[43px] py-[9px] rounded-[5px] text-[22px] font-bold hover:opacity-80 transition-opacity">
                 修正
